7a740d9a63858a8c03f791d6f874edfafb56e276
Jerison <jerisonw@gmail.com>
2024-08-05 01:25:07 -0700

    juiced

--- main.py
+++ main.py
@@ -26,23 +26,42 @@ def mp3download(url, save_path):
     try:
         yt = YouTube(url)
         streams = yt.streams.filter(only_audio=True).first()
-        streams.download(output_path=save_path)
+        out = streams.download(output_path=save_path)
+        base = os.path.splitext(out)
+        new_file = base + ".mp3"
+        os.rename(out, new_file)
         print("Video downloaded successfully!")
     except Exception as e:
         print(e)
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/download', methods =['POST'])
+@app.route('/get_video_details', methods=['POST'])
+def get_vid_detail():
+    url = request.form['url']
+    try:
+        yt = YouTube(url)
+        video_details = {
+            'title': yt.title,
+            'thumbnail': yt.thumbnail_url,
+            'url': url
+        }
+        return render_template('confirm.html', video_details=video_details)
+    except Exception as e:
+        flash(f"An error occurred: {str(e)}")
+        return redirect(url_for('index'))
+    
+@app.route('/download', methods=['POST'])
 def download():
     url = request.form['url']
     file_type = request.form['file_type']
     if not url:
         flash('URL is required!')
         return redirect(url_for('index'))
-    
+
     save_path = app.config['UPLOAD_FOLDER']
     
     if file_type == 'mp4':
@@ -52,6 +71,7 @@ def download():
     
     flash(message)
     return redirect(url_for('index'))
+
 
 if __name__ == '__main__':
     app.run(debug=True)
